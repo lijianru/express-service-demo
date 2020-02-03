48432cddd982f2e5e84ad8fa64b82ff435ebe836
lijianru <2219269374@qq.com>
2020-02-03 21:39:37 +0800

    server

---
+++ server/models/AdminUser.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const schema = new mongoose.Schema({
+    username: { type: String },
+    password: { 
+        type: String,
+        select: false,
+        set(value) {
+            return bcrypt.hashSync(value, 10)
+        }
+    }
+})
+
+module.exports = mongoose.model('AdminUser', schema)
---
+++ server/routes/admin/index.js
@@ -0,0 +1,102 @@
+module.exports = app => {
+    const express = require('express')
+    const inflection = require('inflection')
+    const muter = require('multer')
+    const path = require('path')
+    const AdminUser = require('../../models/AdminUser')
+    const bcrypc = require('bcrypt')
+    const jwt = require('jsonwebtoken')
+    const httpAssert = require('http-assert')
+
+    const router = express.Router({
+        mergeParams: true
+    })
+
+    const verifyUserMiddleware = async (req, res, next) => {
+        const token = String(req.headers.authorization || '').split(' ').pop()
+        // TODO 容错，如果token校验不通过
+        const { id } = jwt.verify(token, app.get('secrte'))
+        const user = await AdminUser.findById(id)
+        httpAssert(user, 422, '用户未登录！')
+
+        await next()
+    }
+
+    // 获取列表
+    router.get('/', verifyUserMiddleware, async (req, res) => {
+        const queryOptions = {}
+        if (req.Model.modelName === 'Category') {
+            queryOptions.populate = 'parent'
+        }
+        // populate('parent') 关联查询 
+        // limit(10) 限制为10条
+        const data = await req.Model.find().setOptions(queryOptions).limit(10)
+
+        res.json(data)
+    })
+
+    // 新增
+    router.post('/', verifyUserMiddleware, async (req, res) => {
+        const model = await req.Model.create(req.body)
+
+        res.send(model)
+    })
+
+    // 更新
+    router.put('/:id', verifyUserMiddleware, async (req, res) => {
+        const data = await req.Model.findById(req.params.id)
+        // TODO 需要区分接口
+        data = req.body
+
+        await data.save()
+
+        res.send(data)
+    })
+
+    // 删除
+    router.delete('/:id', verifyUserMiddleware, async (req, res) => {
+        const data = await req.Model.findById(req.params.id)
+        await data.delete()
+
+        res.send({
+            success: true
+        })
+    })
+
+    app.use('/admin/api/rest/:resource', async (req, res, next) => {
+        // 将动态转递的参数
+        const modelName = inflection.classify(req.params.resource)
+        // TODO 需要做容错，是否存在该模块
+        const Model = require(`../../models/${modelName}`)
+        req.Model = Model
+        next()
+    }, router)
+
+    // 上传
+    const uploadMiddleware = muter({
+        dest: path.resolve(__dirname, '../../uploads')
+    })
+    app.post('/admin/api/upload', uploadMiddleware.single('file'), async (req, res) => {
+        const file = req.file
+        file.url = `http://127.0.0.1:3000/uploads/${file.name}`
+        res.send(file)
+    })
+
+    // 登录
+    app.post('/admin/api/login', async (req, res) => {
+        const { username, password } = req.body
+        const user = await AdminUser.findOne({username}).select('+password')
+        httpAssert(user, 422, '用户不存在！')
+        const isVliad = bcrypc.compareSync(password, user.password)
+        httpAssert(isVliad, 422, '密码不正确！')
+        const token = jwt.sign({ id: user._id }, app.get('secrte'))
+        res.send({ token })
+    })
+
+    // 错误处理
+    app.use(async (err, req, res, next) => {
+        res.status(err.statusCode).send({
+            message: err.message
+        })
+    })
+}
