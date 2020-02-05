cdc10f8b37a648549542b0be9916a81190d31bc1
lijianru <2219269374@qq.com>
2020-02-05 17:46:00 +0800

    update

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,7 +1,12 @@
 module.exports = (app) => {
   const mongoose = require('mongoose');
+  const requireAll = require('require-all');
+  const path = require('path');
+
   mongoose.connect('mongodb://localhost:27017/express-react-lol', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+
+  requireAll(path.resolve(__dirname, '../models'));
 };
